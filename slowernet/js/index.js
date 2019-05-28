b6b642226eb66f9c622975c1c359bef80f4de2f1
Norman <41760554+n0rmancodes@users.noreply.github.com>
2019-05-28 00:03:25 -0400

    new slow connection mode
    no external connections or images in this mode. only basic css, js and html

---
+++ slowernet/js/index.js
@@ -0,0 +1,23 @@
+var s = document.getElementById("search");
+s.addEventListener("keydown", function (e) {
+    if (e.keyCode == 13) {
+        search()
+    } 
+});
+window.onload(onloadall())
+function onloadall() {
+	s.focus();
+	document.getElementById("se").value = localStorage.getItem('ss_engine');
+}
+function search() {
+	var se = document.getElementById("se").value;
+	var query = encodeURIComponent(document.getElementById("search").value);
+	document.getElementById("ajaxloader").style.display = "block";
+	if (se === "ddg") {var search = "https://www.duckduckgo.com/?q=" + query; localStorage.setItem('ss_engine', 'ddg');
+	if (se === "google") {var search = "https://www.google.com/search?q=" + query; localStorage.setItem('ss_engine', 'google');}
+	if (se === "yahoo") {var search = "https://search.yahoo.com/search?p=" + query; localStorage.setItem('ss_engine', 'yahoo');}
+	if (se === "dp") {var search = "https://results.dogpile.com/search/web?q=" + query; localStorage.setItem('ss_engine', 'dp');}
+	if (se === "bing") {var search = "https://www.bing.com/search?q=" + query; localStorage.setItem('ss_engine', 'bing');}
+	if (se === "") {alert("choose a search engine"); document.getElementById("ajaxloader").style.display = "none";}
+	window.open(search, "_self");
+}
