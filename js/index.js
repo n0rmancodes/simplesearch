735d3e3ebbe6a223f2a884a9e8ea0640dfa019e4
Norman <41760554+n0rmancodes@users.noreply.github.com>
2019-10-03 22:08:25 -0400

    removing too high-res images and other things :)

--- js/index.js
+++ js/index.js
@@ -19,7 +19,6 @@ b.addEventListener("keydown", function (e) {
 
 setInterval(function() {
 	genDate();
-	checkRes();
 }, 100);
 
 onloadall()
@@ -278,7 +277,7 @@ function dispSuggested() {
 }
 
 function getBackground() {
-	var backs = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", "img/06.jpg", "img/07.jpg", "img/08.jpg", "img/09.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg", "img/13.jpg", "img/14.jpg", "img/15.jpg", "img/16.jpg", "img/17.jpg", "img/18.jpg", "img/19.jpg", "img/20.jpg", "img/21.jpg", "img/22.jpg", "img/23.jpg", "img/24.jpg", "img/25.jpg", "img/26.jpg", "img/27.jpg", "img/28.jpg", "img/29.jpg", "img/30.jpg", "img/31.jpg", "img/32.jpg", "img/33.jpg", "img/34.jpg", "img/35.jpg", "img/36.jpg", "img/37.jpg", "img/38.jpg", "img/39.jpg", "img/40.jpg", "img/41.jpg", "img/42.jpg", "img/43.jpg", "img/44.jpg", "img/45.jpg", "img/46.jpg"];
+	var backs = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", "img/06.jpg", "img/07.jpg", "img/08.jpg", "img/09.jpg", "img/10.jpg", "img/11.jpg", "img/12.jpg", "img/13.jpg", "img/14.jpg", "img/15.jpg", "img/16.jpg", "img/17.jpg", "img/18.jpg", "img/19.jpg", "img/20.jpg", "img/21.jpg", "img/22.jpg", "img/23.jpg", "img/24.jpg", "img/25.jpg", "img/26.jpg", "img/27.jpg", "img/28.jpg", "img/29.jpg", "img/30.jpg", "img/31.jpg", "img/32.jpg", "img/33.jpg", "img/34.jpg", "img/35.jpg", "img/36.jpg", "img/37.jpg", "img/38.jpg", "img/39.jpg", "img/40.jpg", "img/41.jpg"];
 	var curBack = backs[Math.floor(Math.random() * backs.length)];
 	document.getElementById("backgroundId").innerHTML =('body {background-image: url("'+ curBack + '")}');
 	if (curBack === "img/01.jpg") {var credit = "StockSnap from Pixabay"};
@@ -323,8 +322,7 @@ function getBackground() {
 	if (curBack === "img/40.jpg") {var credit = "skeeze from Pixabay"};
 	if (curBack === "img/41.jpg") {var credit = "Elena Prokofyeva on Unsplash"}
 	if (curBack === "img/42.jpg") {var credit = "ian dooley on Unsplash"}
-	if (curBack === "img/43.jpg" | curBack === "img/44.jpg" | curBack === "img/45.jpg" | curBack === "img/46.jpg") {var credit = "n0rmancodes (me)"}
-   	document.getElementById("pC").innerHTML = credit;
+	document.getElementById("pC").innerHTML = credit;
 }
 
 function linkMode() {
